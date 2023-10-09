8a0b3bfb64fc5a3dd2124f393473dfc4bb1adc9e
William Bojczuk <williambojczuk@gmail.com>
2023-10-09 12:21:25 -0400

    added some reviews

--- src/components/Reviews/reviewData.ts
+++ src/components/Reviews/reviewData.ts
@@ -1,29 +1,27 @@
 const reviewData: ReviewType[] = [
     {
-        name: "Johnny Appleseed",
+        name: "Erin P.",
         rating: 5,
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        desc: `Great customer service, honest guys.
+
+I called SpeedSpray for a quote and estimate. Got an instant response over the phone . They answered immediately and gave their base estimate but also gave some excellent advice and their professional opinion that didn’t require the more expensive treatment. It’s a very rare company that doesn’t try to upsell you, and they were completely correct. Thank you so much.
+        `
     },
     {
-        name: "Johnny Appleseed",
+        name: "Randi H.",
         rating: 5,
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        desc: "From start to finish George was a delight to work with. He made it very easy to get a quote and schedule. Skylar showed up promptly, did an amazing job power washing my front deck, driveway and retaining wall and cleaning the gutters. George took the time to take photos of my neighbor's tree what was growing into my gutters and explained what i need to have done. Everything is sparkling clean and with as little stress and hassle as possible. Everyone was incredibly nice, I would highly recommend them and will be calling them again when things get dirty, as they always do."
     },
     {
-        name: "Johnny Appleseed",
+        name: "Gayle A.",
         rating: 5,
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        desc: "SpeedSpray pressure washing exceeded my expectations. Firstly, their response time and secondly their service and rate for driveway and walkway cleaning was fantastic. All the other companies I reached out to did not respond timely or the rates were outrageous. I met George, the owner personally and he is honest and personable. I Highly recommended SpeedSpray pressure washing  for your home or business."
     },
     {
-        name: "Johnny Appleseed",
+        name: "Michelle B.",
         rating: 5,
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    },
-    {
-        name: "Johnny Appleseed",
-        rating: 5,
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    },
+        desc: "SpeedSpray  is amazing! They responded to my query within two hours, and they charged me a very fair amount. They went above and beyond in their services. Arrived promptly and left the house looking cleaner that she ever did before. Highly recommend. George and Skyler were absolutely awesome."
+    }
 ]
 
 export default reviewData
